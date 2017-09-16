cefdff6925ada689444ee4d929c8642bb43ad44a
Sahil Taneja <sahil@taskbob.com>
2017-09-16 21:24:43 +0530

    some refactoring

--- app/helpers/services/open_weather_map_sao.rb
+++ app/helpers/services/open_weather_map_sao.rb
@@ -14,12 +14,50 @@ module Services
       def get_weather_by_city_id(city_id)
         Rails.logger.info("calling weather api for city id #{city_id}")
         res = get_request("http://api.openweathermap.org/data/2.5/forecast?APPID=#{WeatherAppConfig.OPEN_WEATHER_MAP_API_KEY}&id=#{city_id}&units=metric")
-        JSON.parse(res.body) rescue {"cod" => "500", :errors => ["Unable to parse openweathermap response"]}
+        response = JSON.parse(res.body) rescue {"cod" => "500", :errors => ["Unable to parse openweathermap response"]}
+        build_custom_weather_response(response)
       end
 
       def get_weather_by_city_name(city_name)
         res = get_request("http://api.openweathermap.org/data/2.5/forecast?APPID=#{WeatherAppConfig.OPEN_WEATHER_MAP_API_KEY}&q=#{city_name}&units=metric")
-        JSON.parse(res.body) rescue {"cod" => "500", :errors => ["Unable to parse openweathermap response"]}
+        response = JSON.parse(res.body) rescue {"cod" => "500", :errors => ["Unable to parse openweathermap response"]}
+        build_custom_weather_response(response)
+      end
+
+      # If weather api response changes our clients will not break.
+      def build_custom_weather_response(open_weather_response)
+        response = {:status => open_weather_response["cod"], :errors => [], :weather_infos => []}
+        if open_weather_response["cod"] == "200"
+          response[:city_name] = "#{open_weather_response["city"]["name"]}, #{open_weather_response["city"]["country"]}"
+          open_weather_response["list"].each do |info|
+            # For cached results if some of them are before current time.
+            if info["dt"] > Time.now.to_i
+              weather_info  = {}
+              weather_info[:datetime] = "#{info["dt_txt"]} UTC"
+              main = info["main"]
+              next unless main.present?
+              weather_info[:temp] = {
+                :curr => main["temp"],
+                :min => main["temp_min"],
+                :max => main["temp_max"],
+                :metric => "°C"
+              }
+              weather_info[:humidity] = main["humidity"]
+              weather_info[:more_info] = {
+                :description => (info["weather"].first["description"] rescue ""),
+                :weather_icon => (info["weather"].first["icon"] rescue ""),
+                :wind_speed => ("#{info["wind"]["speed"]} m/s" rescue "")
+              }
+              response[:weather_infos] << weather_info
+            end
+          end
+        elsif open_weather_response["cod"] == "404"
+          response[:errors] << "We could not find for city you were looking for. Please try some other city."
+        else
+          Rails.logger.error("Something went wrong calling openweathermap ERROR: #{open_weather_response["errors"].join(",")}")
+          response[:errors].add("We are sorry something went wrong. Please try again later")
+        end
+        response
       end
 
     end
--- app/models/city.rb
+++ app/models/city.rb
@@ -8,49 +8,13 @@ class City < ApplicationRecord
 
   def self.get_weather(city_id, city_name)
     if city_id.to_i == 0
-      return build_custom_weather_response(Services::OpenWeatherMapSao.get_weather_by_city_name(city_name))
+      return Services::OpenWeatherMapSao.get_weather_by_city_name(city_name)
     else
       # assuming weather report is updated every 6 - 12 hours
       open_weather_response = Rails.cache.fetch("open_weather_response:#{city_id}", :expires_in => 6.hours) do
         Services::OpenWeatherMapSao.get_weather_by_city_id(city_id)
       end
-      return build_custom_weather_response(open_weather_response)
+      return open_weather_response
     end
-  end
-
-  # If weather api response changes our clients will not break.
-  def self.build_custom_weather_response(open_weather_response)
-    response = {:status => open_weather_response["cod"], :errors => [], :weather_infos => []}
-    if open_weather_response["cod"] == "200"
-      response[:city_name] = "#{open_weather_response["city"]["name"]}, #{open_weather_response["city"]["country"]}"
-      open_weather_response["list"].each do |info|
-        # For cached results if some of them are before current time.
-        if info["dt"] > Time.now.to_i
-          weather_info  = {}
-          weather_info[:datetime] = "#{info["dt_txt"]} UTC"
-          main = info["main"]
-          next unless main.present?
-          weather_info[:temp] = {
-            :curr => main["temp"],
-            :min => main["temp_min"],
-            :max => main["temp_max"],
-            :metric => "°C"
-          }
-          weather_info[:humidity] = main["humidity"]
-          weather_info[:more_info] = {
-            :description => (info["weather"].first["description"] rescue ""),
-            :weather_icon => (info["weather"].first["icon"] rescue ""),
-            :wind_speed => ("#{info["wind"]["speed"]} m/s" rescue "")
-          }
-          response[:weather_infos] << weather_info
-        end
-      end
-    elsif open_weather_response["cod"] == "404"
-      response[:errors] << "We could not find for city you were looking for. Please try some other city."
-    else
-      Rails.logger.error("Something went wrong calling openweathermap ERROR: #{open_weather_response["errors"].join(",")}")
-      response[:errors].add("We are sorry something went wrong. Please try again later")
-    end
-    response
   end
 end
